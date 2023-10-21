44c93382dff5478873b14a994af1c5a5825a8d8f
alexxu <xu.gp.alex.cp@gmail.com>
2023-10-21 12:35:29 -0500

    made swift project

---
+++ wordhuntcheater0/wordhuntcheater0/wordhuntcheater0App.swift
@@ -0,0 +1,17 @@
+//
+//  wordhuntcheater0App.swift
+//  wordhuntcheater0
+//
+//  Created by Alex Xu on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct wordhuntcheater0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
